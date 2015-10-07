b590876ddb97b5a0ce6d55ad9ea61dfc5ba7ebbb
Dsaezfaus <dam.dsaez@institutoserpis.org>
2015-10-07 08:20:35 +0200

    Version Eclipse

---
+++ JHolaMundo/src/org/institutoserpis/ed/HolaMundo.java
@@ -0,0 +1,14 @@
+package org.institutoserpis.ed;
+import java.util.*;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Introduzca su nombre ");
+		Scanner tec = new Scanner (System.in);
+		String nombre = tec.nextLine();
+		System.out.println("Hola mundo desde eclipse, "+nombre);
+
+	}
+
+}
---
+++ Lenguaje/HolaMundo.c
@@ -0,0 +1,6 @@
+# include <stdio.h>
+
+int main (void) {
+	printf("¡Hola mundo desde c! \n");
+	return 0;
+}
